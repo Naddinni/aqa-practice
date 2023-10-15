1543e70e4ceb6e1f5fd48efb78cbacc690528738
lozin <lozinskaya.nadia@gmail.com>
2023-10-15 20:07:40 +0300

    lessons 4

---
+++ conditions/switch.js
@@ -0,0 +1,31 @@
+const age = 18
+
+// switch (true) {
+//     case age === 18:
+//     console.log("A person is adult")
+//     case age === 17:
+//         console.log("A person is almost adult")
+//     default:
+//         console.log("A person is NOT adult")
+// }
+// switch (true) {
+//     case age >= 18:
+//         console.log("A person is adult")
+//         break
+//     case age === 18:
+//         console.log("A person is almost adult")
+//         break
+//     default:
+//         console.log("A person is NOT adult")
+// }
+
+switch (age) {
+    case 18:
+        console.log("A person is adult")
+        break
+    case a16:
+        console.log("A person is almost adult")
+        break
+    default:
+        console.log("A person is NOT adult")
+}
---
+++ conditions/ternar.js
@@ -0,0 +1,11 @@
+
+const age = 18
+const message = age >= 18 ? "ADULT" : "TOO YOUNG"
+    // альтернатива з елсе
+let msg
+if(age >= 18){
+    msg = "ADULT"
+}else{
+    msg = "TOO YOUNG"
+}
+console.log(message)
